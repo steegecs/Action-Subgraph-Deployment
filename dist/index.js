221dbdda948a872e0d8eebeca6b85276d4a7da30
Chris Steege <steegecas@gmail.com>
2022-08-31 09:29:10 -0500

    T

--- dist/index.js
+++ dist/index.js
@@ -9003,6 +9003,12 @@ const COMMIT_AFTER_PUSH = core.getInput('COMMIT_AFTER_PUSH')
 console.log(COMMIT_BEFORE_PUSH)
 console.log(COMMIT_AFTER_PUSH)
 
+let scripts = []
+scripts.push("git show " + COMMIT_BEFORE_PUSH + ":" + ABSOLUTE_PATH + "/deployment/deployment.json")
+scripts.push("git show " + COMMIT_AFTER_PUSH + ":" + ABSOLUTE_PATH + "/deployment/deployment.json")
+
+runCommands(scripts, 2, function() {})
+
 // const DEPLOYMENT_CONFIGURATIONS_JSON = require(ABSOLUTE_PATH + "/deployment/deployment.json");
 // const DEPLOYMENT_CONFIGURATIONS = JSON.parse(JSON.stringify(DEPLOYMENT_CONFIGURATIONS_JSON))[
 //     "subgraphs"
